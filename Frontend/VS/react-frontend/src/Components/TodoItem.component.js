7b013c01c4dbdbe01e93117a94a2e6dbb0ec1e8c
Selina Moritz <semoit00@hs-esslingen.de>
2023-06-25 16:46:37 +0200

    frontend fertig

--- Frontend/VS/react-frontend/src/Components/TodoItem.component.js
+++ Frontend/VS/react-frontend/src/Components/TodoItem.component.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react"; 
 import ServiceTodoItem from "../service/ServiceTodoItem.service"
-
+import "../Style/TodoItem.css";
 
 class TodoItem extends Component{
 constructor(props){
@@ -14,17 +14,10 @@ constructor(props){
         this.delete=this.delete.bind(this);
         this.handleChange=this.handleChange.bind(this)
         this.handleSubmit=this.handleSubmit.bind(this)
+        this.update = this.update.bind(this);
       }
       componentDidMount() {
-        ServiceTodoItem.getTodos()
-          .then((response) => {
-            if (Array.isArray(response) && response.length > 0) {
-              this.setState({ todos: response });
-            }
-          })
-          .catch((error) => {
-            console.error("Error retrieving todos:", error);
-          });
+        this.getTodos();
       }
       getTodos(){
         ServiceTodoItem.getTodos()
@@ -41,6 +34,8 @@ constructor(props){
         event.preventDefault()
         let name = event.target.name
         let value = event.target.value
+          // Überprüfung der ID auf Duplikate
+
         this.setState({
             [name]:value
         })
@@ -48,6 +43,22 @@ constructor(props){
     handleSubmit(event){
       event.preventDefault();
       const { id, name, description } = this.state;
+  
+      // Überprüfung der ID auf Duplikate
+      const isIdDuplicate = this.state.todos.some((todo) => todo.id === parseInt(id));
+      if (isIdDuplicate && !(this.state.isEditing) ) {
+        alert("Die ID existiert bereits. Bitte geben Sie eine eindeutige ID ein.");
+        return;
+      }
+      if(id<=0){
+        alert("die ID soll größer gleich 0 sein");
+        return;
+      }
+    
+      if (this.state.isEditing) {
+        // Aufruf der Update-Funktion
+        this.update(id);
+      } else
       ServiceTodoItem.createTodo({
         id: id,
         name: name,
@@ -61,14 +72,39 @@ constructor(props){
           name: "",
           description: "",
         });
+        window.location.reload();
                     })
           .catch((error) => {
             console.error("Fehler beim Erstellen des Todos:", error);
             // Hier können Sie eine Fehlerbehandlung implementieren
           });
-          this.getTodos();
+          
       }
-  
+      update(id) {
+        const { name, description } = this.state;
+        const updatedItem = {
+          name: name,
+          description: description
+        };
+    
+        ServiceTodoItem.updateTodo(id, updatedItem)
+          .then((response) => {
+            // Todo erfolgreich aktualisiert
+            console.log("Todo aktualisiert:", response);
+            // Hier können Sie weitere Aktionen ausführen, z. B. die Anzeige aktualisieren
+            this.setState({
+              id: "",
+              name: "",
+              description: "",
+              isEditing: false // isEditing zurücksetzen
+            });
+            this.getTodos(); // Aktualisierte Todos erneut abrufen
+          })
+          .catch((error) => {
+            console.error("Fehler beim Aktualisieren des Todos:", error);
+            // Hier können Sie eine Fehlerbehandlung implementieren
+          });
+      }
       delete(id) {
         ServiceTodoItem.deleteTodo(id)
           .then(() => {
@@ -80,35 +116,57 @@ constructor(props){
             console.error("Fehler beim Löschen des Todos:", error);
           });
       }
-
+      startEditing(todo) {
+        this.setState({
+          id: todo.id,
+          name: todo.name,
+          description: todo.description,
+          isEditing: true,
+        });
+      }
 
     
     render(){
-        const { todos, id, name, description} = this.state; 
+        const { todos, id, name, description,} = this.state; 
  
         return (
-        <div>
-              <h1>TodoItems</h1>
-              {todos && todos.length > 0 ? (
-              <ul>
-                {todos.map((todo) => (
-                  todo && (
-                    <li className="todo-container" key={todo.id}>
-                      <p>{todo.name}</p>
-                      id: {todo.id} | Beschreibung: {todo.description}
-                      <button
-                        className="deleteButton"
-                        onClick={() => this.delete(todo.id)}
-                      >
-                        Löschen
-                      </button>
-                    </li>
-                  )
-                ))}
-              </ul>
-            ) : (
-              <div>Keine Todos vorhanden</div>
-            )}
+<div className="container">
+  
+  <h1>All TodoItems</h1>
+  <table>
+  <thead>
+    <tr>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Beschreibung</th>
+      <th>Aktionen</th>
+    </tr>
+  </thead>
+  <tbody>
+    {todos.map((todo) => (
+      todo && (
+        <tr key={todo.id}>
+          <td>{todo.id}</td>
+          <td>{todo.name}</td>
+          <td>{todo.description}</td>
+          <td>
+            <button
+              className="deleteButton"
+              onClick={() => this.delete(todo.id)}
+            >
+              Löschen
+            </button>
+            <button className="editButton" onClick={() => this.startEditing(todo)}>
+              Bearbeiten
+            </button>
+          </td>
+        </tr>
+      )
+    ))}
+  </tbody>
+</table>
+<h1>Add / Edit TodoItems</h1>
+
             <form onSubmit={this.handleSubmit}>    
    
              <label>id: </label> 
@@ -118,6 +176,8 @@ constructor(props){
               name="id"
               value={id  || ''} 
               onChange={this.handleChange}
+              readOnly={this.state.isEditing} // Hier wird das readOnly-Attribut basierend auf dem isEditing-Zustand festgelegt
+              
             />   
              <label>Name: </label> 
              <input
